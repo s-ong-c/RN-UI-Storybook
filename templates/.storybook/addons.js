a7b6188b3f8f8e3f6e66f84093f77ea4892a64f3
s-ong-c <somony9292@gmail.com>
2020-07-01 11:42:11 +0900

    feat: error fix

--- templates/.storybook/addons.js
+++
@@ -1,5 +0,0 @@
-import './node_modules/@storybook/addon-actions/register';
-import './node_modules/@storybook/addon-links/register';
-import './node_modules/@storybook/addon-notes/register-panel';
-import './node_modules/@storybook/addon-knobs/register';
-import './node_modules/@storybook/addon-viewport/register';
