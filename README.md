e544b11967af1b2aae85f1893fd975fefb50f5d6
s-ong-c <somony9292@gmail.com>
2020-07-01 11:03:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# REACT-NATIVE UI TEMPLATE with Storybook
---
+++ templates/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function(api) {
+    api.cache(true);
+    return {
+      presets: [
+        'babel-preset-expo',
+        '@babel/preset-typescript',
+      ],
+      plugins: [
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-transform-modules-commonjs",
+      ]
+    };
+  };
